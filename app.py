c996150d7e524ef5f3def254f149495a87ae9cbf
Omer Munk <omermunk95@gmail.com>
2024-10-30 20:04:54 +0700

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 # pip install -r requirements.txt
+from flask import Flask
+from flask_graphql import GraphQLView
+from database import db_session, init_db
+
+
+app = Flask(__name__)
+app.debug = True
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://omermunk:1234@localhost:5432/ww2_operations'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from models import *
 
 
+#todo: can be converted to env variable via os.environ.get('DB_URL')
 connection_url = 'postgresql://omermunk:1234@db:5432/users_subjects_db'
 engine = create_engine(connection_url, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
