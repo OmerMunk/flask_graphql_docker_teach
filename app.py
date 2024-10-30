322c8cf32522f584c874e3d93007a462e4575028
Omer Munk <omermunk95@gmail.com>
2024-10-30 20:09:30 +0700

    with app context

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 # pip install -r requirements.txt
 from flask import Flask
 from flask_graphql import GraphQLView
-from database import db_session, init_db
+from database import db_session, init_db, connection_url
 
 
 app = Flask(__name__)
 app.debug = True
 
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://omermunk:1234@localhost:5432/ww2_operations'
+app.config["SQLALCHEMY_DATABASE_URI"] = connection_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+with app.app_context():
+    init_db()
+
+
+
 
 
