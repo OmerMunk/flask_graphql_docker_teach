a2c955572a42ab23ff659d91dbeaa1bce3155715
Omer Munk <omermunk95@gmail.com>
2024-10-30 22:26:07 +0700

    exercices

--- models.py
+++ models.py
@@ -14,8 +14,7 @@ user_subject_relation = Table(
 class UserModel(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    first_name = Column(String)
-    last_name = Column(String)
+    name = Column(String)
     birth_date = Column(Date)
 
     subjects = relationship (
--- schema.py
+++ schema.py
@@ -2,19 +2,52 @@ import graphene
 from graphene_sqlalchemy import SQLAlchemyObjectType
 
 from database import db_session
-from models import UserModel
+from models import UserModel, SubjectModel
 
 
 class User(SQLAlchemyObjectType):
     class Meta:
         model = UserModel
-        interfaces = (graphene.relay.Node, )
+        interfaces = (graphene.relay.Node,)
+
+
+class Subject(SQLAlchemyObjectType):
+    class Meta:
+        model = SubjectModel
+        interfaces = (graphene.relay.Node,)
+
 
 class Query(graphene.ObjectType):
     user_by_id = graphene.Field(User, id=graphene.Int(required=True))
+    users_by_name = graphene.List(User, name_substring=graphene.String(required=True))
+    subjects_by_name = graphene.List(Subject, name_substring=graphene.String(required=True))
+    users_by_subject = graphene.List(User, subject_id=graphene.Int(required=True))
+    # subjects_by_user
+    # users_by_age
+    # users_by_age_range
 
     def resolve_user_by_id(self, info, id):
         return db_session.query(UserModel).get(id)
+
+    def resolve_users_by_name(self, info, name_substring):
+        substring = f"%{name_substring}%"
+        return db_session.query(UserModel).filter(
+            UserModel.name.ilike(substring)
+        ).all()
+
+    def resolve_subjects_by_name(self, info, name_substring):
+        substring = f"%{name_substring}%"
+        return db_session.query(SubjectModel).filter(
+            SubjectModel.name.ilike(substring)
+        ).all()
+
+    def resolve_users_by_subject(self, info, subject_id):
+        return db_session.query(UserModel).join(
+            UserModel.subjects
+        ).filter(
+            SubjectModel.id == subject_id
+        ).all()
+
 
 
 schema = graphene.Schema(query=Query)
