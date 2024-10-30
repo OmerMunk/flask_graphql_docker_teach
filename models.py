7263e6f4191fd1970143dfd201c0c8a20bc626cb
Omer Munk <omermunk95@gmail.com>
2024-10-30 22:12:16 +0700

    relationship

--- models.py
+++ models.py
@@ -1,10 +1,15 @@
-from sqlalchemy import Column, Integer, String, Date
-
+from flask import session
+from sqlalchemy import Column, Integer, String, Date, Table, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
+user_subject_relation = Table(
+    'user_subject',
+    Column('user_id', Integer, ForeignKey('users.id')),
+    Column('subject_id', Integer, ForeignKey('subjects.id'))
+)
 
 class UserModel(Base):
     __tablename__ = 'users'
@@ -12,3 +17,23 @@ class UserModel(Base):
     first_name = Column(String)
     last_name = Column(String)
     birth_date = Column(Date)
+
+    subjects = relationship (
+        "SubjectModel",
+        secondary=user_subject_relation,
+        back_populates="users"
+    )
+
+
+
+class SubjectModel(Base):
+    __tablename__ = 'subjects'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    # Relationship
+    users = relationship(
+        "UserModel",
+        secondary=user_subject_relation,
+        back_populates="subjects"
+    )
